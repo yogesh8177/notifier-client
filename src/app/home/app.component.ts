22c456628533c3dc0c5fe6664bdb2cac98e4ddb3
Yogesh Jadhav <yogesh.jadhav8177@gmail.com>
2017-02-24 04:06:52 +0530

    Delete app.component.ts

--- src/app/home/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<notification-panel></notification-panel>`,
-})
-export class AppComponent  { name = 'Notifier App'; }
